93b855ea8d9732009e391fb9b9cf4c31e20ec76e
Montek <montekkundan@gmail.com>
2022-10-11 21:13:33 +0530

    update

---
+++ puppeteer/tests/example.test.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+describe('My First Test', () => {
+    it('should launch the browser', async function() {
+
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('https://example.com');
+        await page.waitForSelector('h1');
+
+        await browser.close();
+    })
+
+})
