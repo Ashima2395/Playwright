2fcd6b4ab227d85965967f5cb4876623dc00a1bc
Ashima2395 <115610100+Ashima2395@users.noreply.github.com>
2022-10-13 13:31:39 +0530

    remove changes

--- playwright-nodejs/tests/testing.spec.js
+++ playwright-nodejs/tests/testing.spec.js
@@ -22,3 +22,5 @@ test('homepage has Playwright in title and get started link linking to the intro
 function delay(time) {
   return new Promise(function(resolve) { 
       setTimeout(resolve, time)
+  });
+}
